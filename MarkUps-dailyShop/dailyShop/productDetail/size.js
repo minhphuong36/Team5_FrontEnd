477b4408171f4a67b9915ee999f98e4d204344ea
buivannam1705 <haitac23456789@gmail.com>
2023-02-14 11:13:46 +0700

    kkk

---
+++ MarkUps-dailyShop/dailyShop/productDetail/size.js
@@ -0,0 +1,42 @@
+let isCreate = true;
+
+function ListSize(){
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer' + localStorage.setItem("token")
+        },
+        url: "http://localhost:8080/productDetail",
+        success: function (data) {
+            console.log(data)
+            let str = ""
+            for (const size of data) {
+                str += `<option value={size.id}>${size.name}</option>`
+
+            }
+            document.getElementById("showSize").innerHTML = str;
+        }
+    })
+}showProductDetail();
+
+function createSize(){
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer' + localStorage.setItem("token")
+        },
+        url: "http://localhost:8080/size",
+        success: function (data) {
+            console.log(data)
+            let str = ""
+            for (const size of data) {
+                str +=`<option value={size.id}>${size.name}</option>`
+            }
+
+            document.getElementById("").innerHTML= str;
+        }
+
+    })
+}
